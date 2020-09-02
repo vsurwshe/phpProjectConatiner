65d23df340a25029f8fc0988503b52c165a79607
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-09-02 10:40:47 +0530

    0.0.4: Modify the permission

---
+++ src/api/database/migrations/2020_09_02_050504_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            //these 3 fields added
+            $table->string('username');
+            $table->text('password');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

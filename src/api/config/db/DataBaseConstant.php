b4d19c1f5645895674636d953f3cf06121cdf75e
Vishvanath Surwshe <vsurwshe@gmail.com>
2021-01-11 10:35:11 +0530

    0.4.23: modify the docker compose file and database path

--- src/api/config/db/DataBaseConstant.php
+++ src/api/config/db/DataBaseConstant.php
@@ -1,8 +1,8 @@
 <?php 
 
-define('DB_HOST', 'php_rest_api_db');
+define('DB_HOST', 'webserver_db');
 define('DB_USER', 'vishva');
 define('DB_PASSWORD', 'admin@123');
-define('DB_NAME', 'employee');
+define('DB_NAME', 'vany');
 // define('DB_NAME', 'vany');
 ?>
